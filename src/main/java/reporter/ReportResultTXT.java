0408bf238d96d5bdce8676225a1329354bd2d7bf
Pavlos Anagnostou <paulanagnostou04@gmail.com>
2023-12-07 22:53:31 +0200

    some work on report

--- src/main/java/reporter/ReportResultTXT.java
+++ src/main/java/reporter/ReportResultTXT.java
@@ -1,5 +1,7 @@
 package reporter;
 
+import java.io.File;
+import java.io.FileWriter;
 import java.io.IOException;
 
 import dom2app.SingleMeasureRequestModel;
@@ -8,7 +10,10 @@ public class ReportResultTXT implements ReportResult{
 
 	@Override
 	public int reportToFile(String outputFilePath,SingleMeasureRequestModel request) throws IOException {
-		// TODO Auto-generated method stub
+		File report = new File("Report.txt");
+		FileWriter myWriter = new FileWriter(outputFilePath+"Report.txt");
+		myWriter.write("");
+		myWriter.close();
 		return 0;
 	}
 
